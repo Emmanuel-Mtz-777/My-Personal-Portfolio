ebb75292c9b948a93ced3d80e9df00a03dc09b4c
Emmanuel Martinez <hemmanuelmtz777@gmail.com>
2026-05-30 23:49:28 -0600

    Fix UI to education section

---
+++ src/utils/data/StackData.js
@@ -0,0 +1,61 @@
+import AstroIcon from "../../components/icons/tec/frontend/Astro.astro";
+import Css from "../../components/icons/tec/frontend/Css.jsx";
+import Docker from "../../components/icons/tec/tools/Docker.jsx";
+import Express from "../../components/icons/tec/backend/Express.jsx";
+import Git from "../../components/icons/tec/tools/Git.jsx";
+import Html from "../../components/icons/tec/frontend/Html.jsx";
+import Javascript from "../../components/icons/tec/langs/Javascript.jsx";
+import Laravel from "../../components/icons/tec/backend/Laravel.jsx";
+import Mysql from "../../components/icons/tec/db/Mysql.jsx";
+import Nodejs from "../../components/icons/tec/backend/Node.jsx";
+import ReactIcon from "../../components/icons/tec/frontend/React.jsx";
+import Tailwind from "../../components/icons/tec/frontend/Tailwind.jsx";
+import Expo from "../../components/icons/tec/frontend/Expo.astro";
+import Typescript from "../../components/icons/tec/langs/Typescript.jsx";
+import Vue from "../../components/icons/tec/frontend/Vue.jsx";
+import Java from "../../components/icons/tec/langs/Java.astro";
+import PhP from "../../components/icons/tec/langs/PhP.astro";
+import Python from "../../components/icons/tec/langs/Python.astro";
+import SQLite from "../../components/icons/tec/db/SQLite.astro";
+import SpringBoot from "../../components/icons/tec/backend/SpringBoot.astro";
+import ElasticSearch from "../../components/icons/tec/db/ElasticSearch.jsx";
+import Supabase from "../../components/icons/tec/db/Supabase.astro";
+import Netlify from "../../components/icons/tec/tools/Netlify.astro";
+
+export const frontend = [
+  { icon: Html, title: "HTML" },
+  { icon: Css, title: "CSS" },
+  { icon: Tailwind, title: "Tailwind CSS" },
+  { icon: AstroIcon, title: "Astro" },
+  { icon: Vue, title: "Vue.js" },
+  { icon: ReactIcon, title: "React" },
+  { icon: Expo, title: "Expo" },
+];
+
+export const languajes = [
+  { icon: Javascript, title: "JavaScript" },
+  { icon: Typescript, title: "TypeScript" },
+  { icon: Java, title: "Java" },
+  { icon: PhP, title: "PHP" },
+  { icon: Python, title: "Python" },
+];
+
+export const databases = [
+  { icon: Mysql, title: "MySQL" },
+  { icon: SQLite, title: "SQLite" },
+  { icon: ElasticSearch, title: "ElasticSearch" },
+  { icon: Supabase, title: "Supabase" },
+];
+
+export const backend = [
+  { icon: Nodejs, title: "Node.js" },
+  { icon: Express, title: "Express" },
+  { icon: Laravel, title: "Laravel" },
+  { icon: SpringBoot, title: "Spring Boot" },
+];
+
+export const tools = [
+  { icon: Docker, title: "Docker" },
+  { icon: Git, title: "Git" },
+  { icon: Netlify, title: "Netlify" },
+];
